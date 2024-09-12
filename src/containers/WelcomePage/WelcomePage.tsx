e29d95e7fe2608ea92d6b4f8dbd438d09ca229a6
Josef Webster <joeywebster97@gmail.com>
2024-09-12 19:45:12 +0100

    Added styling to welcomepage

--- src/containers/WelcomePage/WelcomePage.tsx
+++ src/containers/WelcomePage/WelcomePage.tsx
@@ -5,7 +5,9 @@ import './WelcomePage.scss';
 const WelcomePage = () => {
   return (
     <div className='welcome-page-container'>
-        <h1>Welcome To The Final Space Character Database</h1>
+        <div className='welcome-page-container__card'>
+            <h1>Welcome To The Final Space Character Database</h1>
+        </div>
     </div>
   )
 }
